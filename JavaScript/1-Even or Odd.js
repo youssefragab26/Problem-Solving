91c508fb561bcb7d42cd869b0b2882317d67d519
Youssefragab26 <youssef26ragab@gmail.com>
2025-01-07 13:30:20 +0200

    PS

--- JavaScript/1-Even or Odd.js
+++ JavaScript/1-Even or Odd.js
@@ -1,5 +1,6 @@
-//Create a function
-//That takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+// Create a function
+// That takes an integer as an argument and returns
+// "Even" for even numbers or "Odd" for odd numbers.
 
 function evenOrOdd(number) {
     if(number % 2 === 0){
---
+++ JavaScript/8-Number Array.js
@@ -0,0 +1,4 @@
+let LST = [3, 7, 3, 5, 11, 9, 7, 1,]
+// Write Only The Odd Numbers
+// Count Of Repeated Odd Numbers
+// The Largest Numbers Formed From All Odd Numbers
